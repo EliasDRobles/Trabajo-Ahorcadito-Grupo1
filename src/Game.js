caa1abe58be28238ba553bb8e366560e6b5bc741
EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
2022-10-07 22:50:36 -0300

    Actualizacion Desarrolladores. Creacion JSON

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
 import Home from './components/Home';
-import Juego from './components/Juego';
 import Desarrolladores from './components/Desarrolladores';
+import React from 'react';
 
 function Game() {
   return (
@@ -9,7 +9,6 @@ function Game() {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/juego' element={<Juego/>}/>
         <Route path='/desarrolladores' element={<Desarrolladores/>}/>
       </Routes>
     </Router>
